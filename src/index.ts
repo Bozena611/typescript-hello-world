ab475ef01e23eeefa99ffc358b53a939532332f4
Bozena11 <bozena.vuckovic@gmail.com>
2022-08-02 12:07:48 +0200

    Change config for functions

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,17 @@ function render(document: any) {
     console.log(document);
 }
 
+//PascalCase
 enum Size { Small = 1, Medium, Large };
 let mySize: Size = Size.Medium;
 console.log(mySize);
+
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 50_000) {
+        return income * 1.2;
+    } else {
+        return income * 1.3;
+    }
+}
+
+calculateTax(10_000);
