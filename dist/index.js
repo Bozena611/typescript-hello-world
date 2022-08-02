4171334c5dc1aa6e26ad3419506b9c8f059b5422
Bozena11 <bozena.vuckovic@gmail.com>
2022-08-02 11:50:39 +0200

    Use enum

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,18 @@
 "use strict";
-let age = 20;
-if (age < 50) {
-    age += 10;
-    console.log(age);
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
 }
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-let age: number = 20;
-if (age<50) {
-    age+=10;
-    console.log(age);
+let sales = 123_456_789;
+let course = 'TypeScript';
+let is_published = true;
+
+let level;
+
+function render(document: any) {
+    console.log(document);
 }
+
+enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log(mySize);
